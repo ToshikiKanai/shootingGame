95344d7c01c4efd0224e4cf1015b8e689bd8bcc4
tossy <50226151+ToshikiKanai@users.noreply.github.com>
2020-12-21 02:13:55 +0900

    first commit

---
+++ collision.js
@@ -0,0 +1,171 @@
+// 線形四分木空間
+// 空間階層のことをレベルとする
+// 各小空間のことをセルとする。
+
+//オブジェクトの位置(座標)から左上と右下のモートン番号を求める
+//左上と右下のモートン番号から所属するレベルを求める
+//モートン番号とレベルから所属するセルを求める
+//レベルとセル番号からオブジェクトを四分木に追加する
+//四分木をクリアする
+//実際にプロトタイプで使用するメソッドはclear()とaddObj(obj)かな？
+class QuadTree{
+    //QuadTreeクラスのプロパティ
+    constructor(width, height, level){
+        this.width = width;
+        this.height = height;
+        this.level = level;
+        this.data = [null];
+        this.currentLevel = 0;
+
+        while(this.currentlevel < layer){
+            this.expand();
+        }
+        
+        this.objectData = [this.data,this.data,this.data,this.data];
+    }
+
+    clear(){
+        this.objectData[0].fill(null);
+        this.objectData[1].fill(null);
+        this.objectData[2].fill(null);
+        this.objectData[3].fill(null);
+    }
+
+    // 要素をdataに追加する。
+    // 必要なのは、要素と、レベルと、レベル内での番号。
+    /*　引数nは、追加するオブジェクトをあらわす。
+            キャラクター : 0
+            キャラクターショット : １
+            エネミー : 2
+            エネミーショット : 3
+    */
+    addNode(n, node, level, index){
+        //indexはその階層(level)の中での番号
+        const offset = ((4 ** level) -1) / 3;
+        const linearIndex = offset + index; 
+
+        if(level > this.level){
+            level = this.level
+        }
+
+        //セルの初期値はnullであるため、nodeを挿入するセルの上の階層を空配列で初期化する(nullのままにしない)。
+        //空配列で初期化することにより、かくセルに複数のオブジェクトを挿入可能となる
+        //全てのnに対して初期化が必要
+        let parentCellIndex = linearIndex;
+        for(var i = 0; i < 4; i++){
+            while(this.data[i][parentCellIndex] === null){
+                this.data[i][parentCellIndex] = [];
+
+                parentCellIndex = Math.floor((parentCellIndex - 1) / 4);
+            }
+        }
+
+        const cell = this.data[n][linearIndex];
+        cell.push(node);
+    }
+
+    // オブジェクトを線形四分木に追加する。
+    // オブジェクトの座標からモートン番号を計算し、適切なセルに割り当てる。
+    addObj(n, obj){
+        const collider = obj;
+        const size = collider.size;
+        const left = collider.x - size
+        const right = collider.x + size
+        const top = collider.y - size
+        const bottom = collider.y + size
+
+        // モートン番号の計算。
+        const leftTopMorton = this.calc2DMortonNumber(left, top);
+        const rightBottomMorton = this.calc2DMortonNumber(right,bottom);
+
+        // 左上も右下も-1（画面外）であるならば、
+        // レベル0として扱う。
+        // なおこの処理には気をつける必要があり、
+        // 画面外に大量のオブジェクトがあるとレベル0に
+        // オブジェクトが大量配置され、当たり判定に大幅な処理時間がかかる。
+        // 実用の際にはここをうまく書き換えて、あまり負担のかからない処理に置き換えるといい。
+        if(leftTopMorton === -1 && rightBottomMorton === -1){
+            this.addNode(obj, 0, 0);
+            return;
+        }
+
+        // 左上と右下が同じ番号に所属していたら、それはひとつのセルに収まっているということなので、 
+        // 特に計算もせずそのまま現在のレベルのセルに入れる。
+        if(leftTopMorton === rightBottomMorton){
+            this.addNode(obj, this.currentLevel, leftTopMorton);
+            return;
+        }
+
+        // 左上と右下が異なる番号（＝境界をまたいでいる）の場合、所属するレベルを計算する。
+        const level = this.calcLevel(leftTopMorton, rightBottomMorton)
+        // そのレベルでの所属する番号を計算する。
+        // モートン番号の代表値として大きい方を採用する。これは片方が-1の場合、-1でない方を採用したいため。
+        const larger = Math.max(leftTopMorton, rightBottomMorton);
+        const cellNumber = this.calcCell(larger, level);
+
+        //線形四分木に追加する
+        this.addNode(n, obj, level, cellNumber);
+    }
+
+    //-- 以下関数定義 --------------------------------------------------------------------------------------
+
+    // 線形四分木の長さを伸ばす。
+    expand(){
+        const nextLevel = this.currentLevel + 1;
+        const length = (4  ** (nextLevel + 1)) / 3;
+
+        while(this.data.length < length){
+            this.data.push(null);
+        }
+
+        this.currentLevel++;
+    }
+
+    // 16bitの数値を1bit飛ばしの32bitにする。
+    // "|"はビットの論理和を求める演算子c
+    separateBit32(){
+        n = (n|(n<<8))& 0x00ff00ff;
+        n = (n|(n<<4))& 0x0f0f0f0f;
+        n = (n|(n<<2))& 0x33333333;
+        return (n|(n<<1))& 0x55555555;
+    }
+
+    calc2DMortonNumber(x,y){
+        if(x < 0|| y < 0){
+            return -1;
+        }else if(x > this.width || y > this.height){
+            return -1
+        }
+        //空間の中の位置を求める
+        const xCell = Math.floor(x / (this.width / (2 ** this.currentLevel)));
+        const yCell = Math.floor(y / (this.height / (2 ** this.currentLevel)));
+        // x位置とy位置をそれぞれ1bit飛ばしの数にし、
+        // それらをあわせてひとつの数にする。
+        // これがモートン番号となる。
+        return (this.separateBit32(xCell)| (this.separateBit32(yCell)<<1));
+    }
+
+    // オブジェクトの所属レベルを算出する。
+    //XORを取った数を2bitずつ右シフトして、0でない数が捨てられたときのシフト回数を採用する。
+    calcLevel(leftTopMorton, rightBottomMorton){
+        // "^"はXORを求める演算子
+        const xorMorton = leftTopMorton ^ rightBottomMorton;
+        let level = this.calcLevel  - 1;
+        let attachedLevel = this.calcLevel;
+        
+        for(let i=0; level >=0; i++){
+            const flag = (xorMorton >> (i * 2)) & 0x3;
+            if(flag > 0){
+                attachedLevel = level;
+            }
+            level--;
+        }
+        return attachedLevel;
+    }
+
+    // 階層を求めるときにシフトした数だけ右シフトすれば空間の位置がわかる。
+    calcCell(morton, level){
+        const shift = ((this.calcLevel - level) * 2);
+        return morton >> shift;
+    }   
+}
---
+++ main.js
@@ -0,0 +1,430 @@
+// - gloval ------------------------------------------------------------------------
+var screenCanvas, info;
+var run = true;
+// var fps = 1000/30; 単位はms
+var mouse = new Point();
+var ctx; //canvas2d
+var fire = false;
+var counter = 0;
+var message;
+var score = 0;
+
+// - const ------------------------------------------------------------------------
+var CHARA_COLOR = "rgba(0, 0, 255, 0.75)";
+var CHARA_SHOT_COLOR = "rgba(0, 255, 0 ,0.75)";
+var CHARA_SHOT_MAX_COUNT = 10;
+var ENEMY_COLOR = "rgba(255, 0, 0, 0.75)";
+var ENEMY_MAX_COUNT = 10;
+var ENEMY_SHOT_COLOR = "rgba(255, 0, 255, 0.75)"
+var ENEMY_SHOT_MAX_COUNT = 100;
+var BOSS_COLOR = "rgba(128, 128, 128, 0.75)";
+var BOSS_BIT_COLOR = "rgba(64, 64, 64, 0.75)";
+var BOSS_BIT_COUNT = 5;
+var BOSS_TIME = 500;
+
+// - main ------------------------------------------------------------------------
+//window.onload()メソッドは、DOMツリー構造および関連リソースが読み込まれたタイミングで実行される
+//window.addEventListener('DOMContentLoaded', function(){}の方がwindow.onloadよりも早く実行される
+window.onload = function(){
+    //-- 各オブジェクト、プロパティの設定。この部分の処理が行われるのは一度だけ------------------------------------------
+    //gloval valiable
+    var i, j;
+    var p = new Point(); //Point()は座標を表すオブジェクト
+    var qTree;
+
+    //index.htmlの"screem"idをもつcanvas要素への参照を取得
+    screenCanvas = document.getElementById("screen"); 
+    screenCanvas.width = 256*2;
+    screenCanvas.height = 256*2;
+
+    //2dcontext
+    ctx = screenCanvas.getContext("2d");
+
+    //線形四分木空間の用意
+    qTree = new QuadTree(screenCanvas.width, screenCanvas.height, 3);
+
+    //target.addEventListener()で(マウスの位置の検出などの)イベントを登録することができる
+    screenCanvas.addEventListener("mousemove", mouseMove, true);
+    screenCanvas.addEventListener("mousedown", mouseDown, true);
+    window.addEventListener("keydown", keyDown, true);
+
+    //index.htmlのp要素への参照を取得
+    //pタグの中身を書き換えてスコアやマウスの座標情報をユーザに表示させる
+    info = document.getElementById("info"); 
+
+    //キャラクターオブジェクトを生成
+    //ユーザはこのオブジェクトを操作してゲームをプレイする
+    //ここではオブジェクトを生成しているだけで画面には表示されない
+    var chara = new Character();
+    chara.init(10);//キャラクターオブジェクトのサイズを設定
+
+    //キャラクターオブジェクトのショット(弾)オブジェクトを格納する配列を用意する
+    var charaShot = new Array(CHARA_SHOT_MAX_COUNT);
+    for(i = 0; i<CHARA_SHOT_MAX_COUNT; i++){
+        //ショット(弾)オブジェクトをCHARA_SHOT_MAX_COUNTの値(個数)だけ生成
+        //ここではオブジェクトを生成しているだけで画面には表示されない
+        charaShot[i] = new CharacterShot();　
+    }
+
+   //エネミーオブジェクトを格納する配列を用意すいる
+   var enemy = new Array(ENEMY_MAX_COUNT);
+   for(i = 0; i<ENEMY_MAX_COUNT; i++){
+       //エネミーオブジェクトをENEMY_MAX_COUNTの値(個数)だけ生成
+       //ここではオブジェクトを生成しているだけで画面には表示されない
+       enemy[i] = new Enemy(); 
+   } 
+
+   //エネミーオブジェクトのショット(弾)オブジェクトを格納する配列を用意する
+   var enemyShot = new Array(ENEMY_SHOT_MAX_COUNT);
+   for(i = 0; i<ENEMY_SHOT_MAX_COUNT; i++){
+       //ショット(弾)オブジェクトをENEMY_SHOT_MAX_COUNTの値(個数)だけ生成
+        //ここではオブジェクトを生成しているだけで画面には表示されない
+       enemyShot[i] = new EnemyShot();
+   }
+
+   //大ボスオブジェクトを生成
+   var boss = new Boss();
+   //小ボスオブジェクトを生成
+   var bit  = new Array(BOSS_BIT_COUNT);
+   for(i  = 0; i<BOSS_BIT_COUNT; i++){
+       bit[i] = new Bit();
+   }
+
+   //キャラクターの初期位置を設定。ここでは画面下部中央に表示されるようにしている
+   mouse.x = screenCanvas.width / 2;
+   mouse.y = screenCanvas.height -20;
+
+   //-- 各オブジェクト、プロパティの設定は以上------------------------------------------------------------------------
+
+    // - call roop ------------------------------------------------------------------------
+    (function(){
+        //この関数の処理が繰り返される
+        //ゲーム開始時からの時間経過をcounter関数で記録。
+        //30fpsの場合、「counter % 30」で時間[s]を求めることができる
+        counter++;
+
+        //ctxは2dcontextオブジェクト
+        //canvas要素への描画はctxの(プロパティや)メソッドを介して行われる
+        //canvas要素内をクリアする
+        ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
+
+        //-- キャラクターの描画処理開始 ----------------------------------------------------------
+        ctx.beginPath();
+
+        chara.position.x = mouse.x;
+        chara.position.y = mouse.y;
+
+        //円形のキャラクターを描画
+        // ctx.arc(chara.position.x, chara.position.y, chara.size, 0, Math.PI * 2, false);
+
+        //三角形のキャラクターを描画
+            //頂点を指定
+        ctx.moveTo(mouse.x, mouse.y - chara.size * Math.sqrt(3) / 2);
+        ctx.lineTo(mouse.x + chara.size, mouse.y + chara.size * Math.sqrt(3) / 2);
+        ctx.lineTo(mouse.x - chara.size, mouse.y + chara.size * Math.sqrt(3) / 2);
+        ctx.closePath();
+            //塗りつぶしカラーを指定
+        ctx.fillStyle = CHARA_COLOR;
+            //塗りつぶす
+        ctx.fill();
+        //-- キャラクターの描画処理は以上 ----------------------------------------------------------
+
+        //キャラクターのショット生成
+        //canvas内でマウスがクリックされるとfireがtrueとなる
+        if(fire){
+            //search all my shot
+            for(i = 0; i<CHARA_SHOT_MAX_COUNT; i++){
+                if(!charaShot[i].alive){
+                    charaShot[i].set(chara.position, 3, 3);//位置、サイズ、スピードを設定
+                    break;//一度のクリックに対して発射する玉は1つなので、一度上記の処理が実行された時点でfor文を抜ける必要がある
+                }
+            }
+            fire = false;
+        }
+
+        //-- キャラクターのショット描画処理開始 ----------------------------------------------------------
+        ctx.beginPath();
+        for(i = 0; i<CHARA_SHOT_MAX_COUNT; i++){
+            if(charaShot[i].alive){
+                charaShot[i].move();//charaShot[i]のy座標をスピードとして指定された値だけ上に移動させる
+
+                //円を描画
+                ctx.arc(
+                    charaShot[i].position.x,
+                    charaShot[i].position.y,
+                    charaShot[i].size,
+                    0, Math.PI * 2, false
+                );
+
+                ctx.closePath();
+            }
+        }
+        ctx.fillStyle = CHARA_SHOT_COLOR;
+        ctx.fill();
+        //-- キャラクターのショット描画処理は以上 ----------------------------------------------------------
+
+        //-- シーンの分岐 ------------------------------------------------------------------------------
+        switch(true){
+            case counter < 150:
+                message = "READY...";
+                break;
+            
+            case counter < 200:
+                message = "GO!!";
+                break;
+
+            default:
+                message = "";
+
+                //-- エネミーの位置などの設定 ------------------------------------------
+                if(counter % 100 === 0 && counter < BOSS_TIME){
+                    if(counter % 100 === 0){
+                    for(i = 0; i<ENEMY_MAX_COUNT; i++){
+                        if(!enemy[i].alive){
+                            j = (counter % 200) / 100;
+
+                            var enemySize = 15;
+                            p.x = -enemySize + (screenCanvas.width + enemySize * 2) * j;
+                            p.y = screenCanvas.height / 2;
+
+                            enemy[i].set(p, enemySize, j);//.set()が呼び出されるとaliveプロパティはtrueとなる
+                            break;
+                            }
+                        }
+                    }
+                }else if(counter === BOSS_TIME){
+                    p.x = screenCanvas.width / 2;
+                    p.y = -80;
+                    boss.set(p, 50, 30)
+
+                    for(i = 0; i<BOSS_BIT_COUNT; i++){
+                        j = 360 / BOSS_BIT_COUNT;
+                        bit[i].set(boss, 15, 5, i * j);
+                    }
+                }
+                //-- エネミーの位置などの設定は以上 ------------------------------------------
+        }
+        //-- シーンの分岐は以上 ------------------------------------------------------------------------------
+
+        //-- エネミーの描画処理開始 ---------------------------------------------------------------------------
+        ctx.beginPath();
+        for(i = 0; i<ENEMY_MAX_COUNT; i++){
+            if(enemy[i].alive){
+                enemy[i].move();
+                ctx.arc(
+                    enemy[i].position.x,
+                    enemy[i].position.y,
+                    enemy[i].size,
+                    0, Math.PI * 2, false
+                );
+
+                if(enemy[i].param % 30 === 0){
+                    for(j = 0; j<ENEMY_SHOT_MAX_COUNT; j++){
+                        if(!enemyShot[j].alive){
+                            p = enemy[i].position.distance(chara.position);
+                            p.normalize();
+                            enemyShot[j].set(enemy[i].position, p, 5, 2);
+
+                            break;
+                        }
+                    }
+                }
+
+                ctx.closePath();
+            }
+        }
+        ctx.fillStyle = ENEMY_COLOR;
+        ctx.fill();
+        //-- エネミーの描画処理開始は以上 -------------------------------------------------------------------------
+
+        //-- エネミーのショット描画処理開始 ---------------------------------------------------------------------------
+        ctx.beginPath();
+        for(i = 0; i<ENEMY_SHOT_MAX_COUNT; i++){
+            if(enemyShot[i].alive){
+                enemyShot[i].move();
+
+                ctx.arc(
+                    enemyShot[i].position.x,
+                    enemyShot[i].position.y,
+                    enemyShot[i].size,
+                    0, Math.PI * 2, false
+                );
+
+                ctx.closePath();
+            }
+        }
+        ctx.fillStyle = ENEMY_SHOT_COLOR;
+        ctx.fill();
+        //-- エネミーのショット描画処理は以上 -------------------------------------------------------------------------
+
+        //-- ボスの描画処理開始 -------------------------------------------------------------------------------------
+            //-- 大ボスの描画処理開始 ----------------------------------------------------------
+        ctx.beginPath();
+        if(boss.alive){
+            boss.move();
+            
+            ctx.arc(
+                boss.position.x,
+                boss.position.y,
+                boss.size,
+                0, Math.PI * 2, false
+            );
+
+            ctx.closePath();
+        }
+        ctx.fillStyle = BOSS_COLOR;
+        ctx.fill();
+            //-- 大ボスの描画処理は以上 ----------------------------------------------------------
+
+            //-- 小ボスの描画処理開始 ----------------------------------------------------------
+        ctx.beginPath();
+        for(i = 0; i<BOSS_BIT_COUNT;i++){
+            if(bit[i].alive){
+                bit[i].move();
+
+                ctx.arc(
+                    bit[i].position.x,
+                    bit[i].position.y,
+                    bit[i].size,
+                    0, Math.PI * 2, false
+                );
+
+                if(bit[i].param % 60 === 0){
+                    //小ボスのショットは通常のエネミーと共通
+                    for(j = 0; i<ENEMY_SHOT_MAX_COUNT; j++){
+                        if(!enemyShot[j].alive){
+                            p = bit[i].position.distance(chara.position);
+                            p.normalize();
+                            enemyShot[j].set(bit[i].position, p, 4, 1.5);
+
+                            break;
+                        }
+                    }
+                }
+
+                ctx.closePath();
+            }
+        }
+        fillStyle = BOSS_BIT_COLOR;
+        ctx.fill();
+            //-- 小ボスの描画処理は以上 ----------------------------------------------------------
+        //-- ボスの描画処理は以上 ------------------------------------------------------------------------------------
+
+
+        //-- 衝突判定 ----------------------------------------------------------------------------------------------
+        //ショットと敵orキャラクター間の距離をもとに衝突判定を行う
+        //-- キャラクターショットの衝突判定 ----------------------------------------------------------------------------
+        //存在する全てのキャラクターショット1つひとつに対して、生存している全ての敵との距離を計算し衝突判定を行っている→計算量O(n^2)
+        for(i = 0; i<CHARA_SHOT_MAX_COUNT; i++){
+            if(charaShot[i].alive){
+                //-- エネミーとの衝突判定 ------------------------------------
+                for(j = 0; j<ENEMY_MAX_COUNT; j++){
+                    if(enemy[i].alive){
+                        p = enemy[j].position.distance(charaShot[i].position);
+                        if(p.length() <= enemy[j].size){
+                            enemy[j].alive = false;
+                            charaShot[i].alive = false;
+
+                            score++;
+                            
+                            break;
+                        }
+                    }
+                }
+
+                //-- 小ボスとの衝突判定 ------------------------------------
+                for(j = 0; j<BOSS_BIT_COUNT; j++){
+                    if(bit[j].alive){
+                        p = bit[j].position.distance(charaShot[i].position);
+                        if(p.length() < bit[j].size){
+                            bit[j].life--;
+                            
+                            charaShot[i].alive = false;
+
+                            if(bit[j].life < 0){
+                                bit[j].alive = false;
+                                score += 3;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                //-- 大ボスとの衝突判定 ------------------------------------
+                if(boss.alive){
+                    p = boss.position.distance(charaShot[i].position);
+                    if(p.length()< boss.size){
+                        boss.life--;
+
+                        charaShot[i].alive = false;
+
+                        if(boss.life < 0){
+                            score += 10;
+                            run = false;
+                            message = "CLEAR!!!!!";
+                        }
+                    }
+                }
+            }
+        }
+        //-- キャラクターショットの衝突判定は以上 --------------------------------------------------------------------
+
+        //-- エネミーショットの衝突判定 ----------------------------------------------------------------------------
+        //エネミーと小ボスともにショットオブジェクトは共通(enemyShot[i])なので、enemyShot[i]とキャラクターオブジェクトとの衝突判定のみ行えばOK
+        for(i = 0; i<ENEMY_SHOT_MAX_COUNT; i++){
+            if(enemyShot[i].alive){
+               p = chara.position.distance(enemyShot[i].position);
+               if(p.length() < chara.size){
+                   charaShot.alive = false;
+
+                   run = false;
+                   info.style.color = "#ff0000";
+                   message = "GAME OVER !!";
+                   break;
+               }
+            }
+        }
+        //-- エネミーショットの衝突判定は以上 ----------------------------------------------------------------------------
+
+       
+
+        info.innerHTML = "SCORE: " + (score * 100) + " " + message;
+        //update HTML 
+        //info.innerHTML = mouse.x + ":" + mouse.y;
+
+        //runフラグを用いて再起呼び出し
+        if(run){
+            //calleeはargumentsオブジェクトのプロパティで、関数内部で実行中の関数を参照するために用いられる。
+            //バグの原因になりやすいため非推奨
+            requestAnimationFrame(arguments.callee);
+            //setTimeout(arguments.callee, fps);
+        }
+    })();
+}
+
+//event
+function mouseMove(event){
+    //recursion mouse-point
+    mouse.x = event.clientX - screenCanvas.offsetLeft;
+    mouse.y = event.clientY - screenCanvas.offsetTop;
+}
+
+function keyDown(event){
+    //get keycode
+    var ck = event.keyCode;
+
+    //escキーが押された場合
+    if(ck === 27){
+        run = false;
+    }
+    //スペースキーが押された場合
+    if(ck === 32 && counter % 2 == 0){
+        fire = true;
+    }
+}
+
+function mouseDown(event){
+    // up flag
+    fire = true;
+}
